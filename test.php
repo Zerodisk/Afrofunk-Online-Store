ed19a156fc6c720fc22c29b9be3c79cbfaecd552
Tan Tangtrakulwongse <zerodisk@yahoo.com>
2012-11-30 17:13:47 +1100

    come back from holiday

--- test.php
+++ test.php
@@ -2,27 +2,25 @@
 date_default_timezone_set('Australia/NSW');
 echo date('Y-m-d H:i:s');
 
+echo phpinfo(); 
+/*
 $includeParam = array('sku','product_name','description','url','original_url','image_url','price','delivery_cost','currency_code','brand','colour','gender','size');
+
 if (in_array('product_name ', $includeParam)) {
+	
 	echo('found it');
+
 }
 else{
 	echo('not found!');
 }
-
-
-try {
-	$i = 1/0;
-}
-catch (Exception $e) {
-	echo('Caught exception: '.$e->getMessage());
-}
-
-//echo phpinfo(); 
+*/
 
 
 
-//echo file_get_contents("http://www.google.com");
+
+
+
 
 
 
