f65440b52b452273d5d448fec82b7874252a9521
TanNovo <zerodisk@hotmail.com>
2013-01-16 17:15:56 +1100

    new admin email page

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -5,7 +5,10 @@
 			<ul>
 				<li class="menu"><a href="<?php echo base_url();?>admin/home">dashboard</a></li>
 				<li class="menu"><a href="<?php echo base_url();?>admin/product">product</a></li>
+				<!-- 
 				<li class="menu"><a href="<?php echo base_url();?>admin/setting">setting</a></li>
+				-->
+				<li class="menu"><a href="<?php echo base_url();?>admin/email/simpleEmail">email</a></li>
 				<li class="menu"><a href="<?php echo base_url();?>admin/logout">logout</a></li>
 			</ul>
 		</div>
