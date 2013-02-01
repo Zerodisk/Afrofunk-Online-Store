20ce674f2207ae72dcf975acc8dfa3365a0fad01
Zerodisk <zerodisk@yahoo.com>
2013-02-02 10:39:03 +1100

    new photo on ad

--- application/views/ads.php
+++ application/views/ads.php
@@ -80,7 +80,7 @@ switch($menu){
 				<img src="/store/images/newyear.jpg" />
 			</div>
 			<div id="ads">		
-				<a href="https://track.commissionfactory.com.au/b/6718/10175/women/special-price/" rel="noindex,nofollow"><img style="border: none; vertical-align: middle;" alt="" src="http://content.commissionfactory.com.au/Banners/9f772a1f-b8f6-4ccb-94b6-607ffa8e72a5/a272e586-baf4-4649-8c97-075906cb1e34.gif" /></a>
+				<a href="https://track.commissionfactory.com.au/b/6718/10486/women/accessories/sunglasses/" rel="noindex,nofollow"><img style="border: none; vertical-align: middle;" alt="" src="http://content.commissionfactory.com.au/Banners/9f772a1f-b8f6-4ccb-94b6-607ffa8e72a5/0ac38747-d568-43bc-a41b-a86505abe760.gif" /></a>
 				<br><br>
 				<a href="https://track.commissionfactory.com.au/b/6718/9139/women/clothing/new-products/?sort=popularity&amp;dir=desc" rel="noindex,nofollow"><img style="border: none; vertical-align: middle;" alt="" src="http://content.commissionfactory.com.au/Banners/9f772a1f-b8f6-4ccb-94b6-607ffa8e72a5/b04260b9-88e1-4106-9f31-ca13a1e0f61e.gif" /></a>
 			</div>
