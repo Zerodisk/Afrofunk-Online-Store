c10398ae191b873cbe7ea377f87202ed7bfea12f
TanNovo <zerodisk@hotmail.com>
2013-02-04 19:39:45 +1100

    add facebook like button on top header

--- application/views/header.php
+++ application/views/header.php
@@ -32,6 +32,17 @@
 				
 </script>
 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-like" data-href="http://www.facebook.com/afrofunkclothing" data-send="false" data-width="450" data-show-faces="false" data-font="arial"></div>
+
 <div id="banner" onclick="window.location.href='/store'" style="cursor:hand">
 	<!--  afrofunk logo  -->		
 </div>
