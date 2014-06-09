4c23479d52acde9fda6bebc9a7f6dda080810f31
TanNovo <zerodisk@hotmail.com>
2014-06-09 21:30:18 +1000

    new side bar banner

--- application/views/ads.php
+++ application/views/ads.php
@@ -80,12 +80,9 @@ switch($menu){
 				<img src="/store/images/bannertop.jpg" />
 			</div>
 			<div id="ads">		
-				<!-- <a href="https://track.commissionfactory.com.au/b/6718/12025/aff_c" rel="noindex,nofollow"><img style="border: none; vertical-align: middle;" alt="" src="http://content.commissionfactory.com.au/Banners/3222eb0a-3271-46e0-b044-0131926cda7c/a55e4eec-e845-4417-9d90-a3c21223bb80.jpg" /></a>  -->
-				<a href="https://track.commissionfactory.com.au/b/6718/14154/boutique/" rel="noindex,nofollow"><img style="border: none; vertical-align: middle;" alt="" src="http://content.commissionfactory.com.au/Banners/9f772a1f-b8f6-4ccb-94b6-607ffa8e72a5/56da36ec-3bdf-4455-a619-74011b8e20f6.gif" /></a>
-				<br><br>
-				
-				<!-- <a href="https://track.commissionfactory.com.au/b/6718/10963/women/clothing/coats-and-jackets/" rel="noindex,nofollow"><img style="border: none; vertical-align: middle;" alt="" src="http://content.commissionfactory.com.au/Banners/9f772a1f-b8f6-4ccb-94b6-607ffa8e72a5/250edc51-d64b-4ec4-9f21-278457c47298.gif" /></a> -->
-				<a href="https://track.commissionfactory.com.au/b/6718/13676/sass-bide/" rel="noindex,nofollow"><img style="border: none; vertical-align: middle;" alt="" src="http://content.commissionfactory.com.au/Banners/9f772a1f-b8f6-4ccb-94b6-607ffa8e72a5/a63e35a4-a0f9-496c-80d0-ef021cafcd69.jpg" /></a>
+				<a href="https://track.commissionfactory.com.au/t/6718/3097/" rel="noindex,nofollow"><img style="border: none; vertical-align: middle;" alt="" src="/store/images/ads-side-1.png" /></a>
+				<br>
+				<a href="https://track.commissionfactory.com.au/t/6718/3097/" rel="noindex,nofollow"><img style="border: none; vertical-align: middle;" alt="" src="/store/images/ads-side-2.png" /></a>
 			</div>
 		</div>
 <?php 
